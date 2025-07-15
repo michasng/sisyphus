a020af01a18457a8c341d549d1632321aa010c4f
Micha Sengotta <micha.sengotta@gmail.com>
2025-07-15 18:19:49 +0200

    Fix unwanted boulder movement

--- scripts/boulder.gd
+++ scripts/boulder.gd
@@ -1,20 +1,20 @@
 class_name Boulder
-extends CharacterBody2D
+extends StaticBody2D
 
 
 @onready var push_velocity_pixels_per_second := 0.5 * Game.TILE_SIZE_PIXELS
 
 var is_pushed := false
+var velocity_pixels_per_second: Vector2 = Vector2.ZERO
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if is_pushed:
-		velocity = Vector2(0, -push_velocity_pixels_per_second)
+		velocity_pixels_per_second = Vector2(0, -push_velocity_pixels_per_second)
 	else:
-		velocity = Vector2.ZERO
-		return
+		velocity_pixels_per_second = Vector2.ZERO
 
-	move_and_slide()
+	move_and_collide(velocity_pixels_per_second * delta)
 
 
 func _on_player_moved(_velocity: Vector2, is_pushing: bool) -> void:
