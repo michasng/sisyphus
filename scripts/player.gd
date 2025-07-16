713bc4d7184d281030db6204bd3cd21a155346f9
Micha Sengotta <micha.sengotta@gmail.com>
2025-07-16 20:59:22 +0200

    Make the player a little bit faster

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,7 @@ enum State {
 @export var state := State.IDLE
 
 @export var walk_velocity_pixels_per_second := 3.0 * Game.TILE_SIZE_PIXELS
-@export var sprint_velocity_pixels_per_second := 4.0 * Game.TILE_SIZE_PIXELS
+@export var sprint_velocity_pixels_per_second := 4.5 * Game.TILE_SIZE_PIXELS
 @export var max_stamina := 3
 @export var stamina_recharge_per_second := 0.5
 @export var push_stamina_consumption_per_second := 0.5
