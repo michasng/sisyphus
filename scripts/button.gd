949b328b6387e63433a7f28747ddbf6cae462b79
Micha Sengotta <micha.sengotta@gmail.com>
2025-07-18 15:33:14 +0200

    Add gate with two buttons

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,26 +1,43 @@
 class_name ButtonTile
 extends Area2D
 
+@export var can_be_released := false
+
 @onready var _up_sprite: Sprite2D = $UpSprite2D
 @onready var _down_sprite: Sprite2D = $DownSprite2D
 @onready var _press_sound_effect: SoundEffect = $PressSoundEffect
+@onready var _release_sound_effect: SoundEffect = $ReleaseSoundEffect
 
 var is_pressed := false:
 	set(value):
+		if value == is_pressed:
+			return
 		is_pressed = value
 		_up_sprite.visible = not is_pressed
 		_down_sprite.visible = is_pressed
 		if is_pressed:
 			_press_sound_effect.play()
-			pressed.emit()
+		else:
+			_release_sound_effect.play()
+		is_pressed_changed.emit(is_pressed)
 
-signal pressed
+signal is_pressed_changed(is_pressed: bool)
 
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	if not is_pressed:
-		is_pressed = true
+	is_pressed = true
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	if can_be_released:
+		is_pressed = false
+
+
+func on_gate_opened() -> void:
+	can_be_released = false
+	is_pressed = true
--- scripts/gate.gd
+++ scripts/gate.gd
@@ -1,6 +1,25 @@
 class_name Gate
 extends StaticBody2D
 
+@export var required_buttons_pressed_count := 1
 
-func _on_button_pressed() -> void:
-	queue_free()
+@onready var open_sound_effect: SoundEffect = $OpenSoundEffect
+var _buttons_pressed_count := 0
+
+signal opened
+
+
+func _on_button_is_pressed_changed(is_pressed: bool) -> void:
+	if is_pressed:
+		_buttons_pressed_count += 1
+	else:
+		_buttons_pressed_count -= 1
+	
+	if _buttons_pressed_count >= required_buttons_pressed_count:
+		# gate "opens" by effectively disappearing
+		visible = false
+		collision_layer = 0
+		collision_mask = 0
+		# the node is not freed, because then the sound effect wouldn't play
+		open_sound_effect.play()
+		opened.emit()
