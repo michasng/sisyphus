f4e82aa7c94f6a5caa62dd2bacc375a118141f28
Micha Sengotta <micha.sengotta@gmail.com>
2025-07-20 11:31:05 +0200

    Add bat flap sound

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -6,15 +6,20 @@ extends CharacterBody2D
 @export var follow_from_distance_pixels := Game.TILE_SIZE_PIXELS * 5
 @export var velocity_pixels_per_second := 1.0 * Game.TILE_SIZE_PIXELS
 
+@onready var _flap_sound_effect: SoundEffect = $FlapSoundEffect
+
+var is_following_target := false
+
 
 func _physics_process(_delta: float) -> void:
 	if not target:
 		return
 	
 	var distance = global_position.distance_to(target.global_position)
-	if distance > follow_from_distance_pixels:
+	is_following_target = distance <= follow_from_distance_pixels
+	if not is_following_target:
 		return
-	
+
 	var direction = global_position.direction_to(target.global_position)
 	velocity = direction * velocity_pixels_per_second
 	move_and_slide()
@@ -22,3 +27,8 @@ func _physics_process(_delta: float) -> void:
 
 func _on_hurt_box_area_entered(_area: Area2D) -> void:
 	queue_free()
+
+
+func _on_animation_looped() -> void:
+	if is_following_target:
+		_flap_sound_effect.resume()
