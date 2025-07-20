0df29c52fd155f2fc62709150a9417dd08948bdd
Micha Sengotta <micha.sengotta@gmail.com>
2025-07-20 10:56:09 +0200

    Add hint to press any key to start

--- scripts/title.gd
+++ scripts/title.gd
@@ -2,11 +2,12 @@ class_name Title
 extends Node2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	animated_sprite.play()
-
+	await get_tree().create_timer(3.0).timeout
+	animation_player.play("label_flash")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey or InputEventJoypadButton and event.is_pressed():
